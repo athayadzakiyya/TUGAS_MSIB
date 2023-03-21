bdf4519c38846de550b0945c18b3d332e5887fde
athayadzakiyya <athayadzakiyya@gmail.com>
2023-03-22 01:30:39 +0700

    up tugas1

--- Tugas1.php
+++ Tugas1.php
@@ -6,26 +6,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tugas 1</title>
 </head>
-<body bgcolor="pink">
-    <H1>Perhitungan Luas Jajargenjang</H1>
+<body bgcolor="pink" align="center">
+
     <form method="post">
-        <table>
-            <tr>
-                <td>Alas</td>
-                <td> <input type="text" name="alas" require> </td>
-            </tr>
-            <tr>
-                <td>Tinggi</td>
-                <td> <input type="text" name="tinggi" require> </td>
-            </tr>
-            <tr>
-                <td><input type="submit" name="submit" value="Hasil"></td>
-            </tr>
-        </table>
+    <table border="3" align="center" cellpadding="10" width="25%">
+
+            <thead>
+                <tr bgcolor="white">
+                    <th colspan="2">Perhitungan Luas Jajargenjang</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <tr>
+                        <td> Alas </td>
+                        <td><input type="text" name="alas" placeholder="masukan angka" require></td>
+                    </tr>
+                    <tr>
+                        <td>Tinggi</td>
+                        <td><input type="text" name="tinggi" placeholder="masukan angka" require></td>
+                    </tr>
+                    <tr>
+                        <td>Sisi Miring</td>
+                        <td><input type="text" name="sisi" placeholder="masukan angka" require></td>
+                    </tr>
+            </tbody>
+
+            <tfoot bgcolor="white">
+                    <td colspan="2" align="center">
+                    <button type="submit" name="luas">Luas</button>&nbsp
+                    <button type="submit" name="keliling">Keliling</button>
+                    </td>
+            </tfoot>
+</table>
     </form>
 
     <?php 
-    if(isset($_POST['submit'])){
+    if(isset($_POST['luas'])){
         $alas = $_POST['alas'];
         $tinggi = $_POST['tinggi'];
 
@@ -37,6 +55,18 @@
         echo '<br> Tinggi = '.$tinggi;
 
         echo '<br> Maka Luasnya adalah ' .$luas;
+    } elseif(isset($_POST['keliling'])){
+        $alas = $_POST['alas'];
+        $sisi = $_POST['sisi'];
+
+        $keliling = 2 * ($alas + $sisi);
+        echo '<hr>';
+        echo 'Hasil perhitungan dari Keliling Jajargenjang';
+        echo '<br> Diketahui :';
+        echo '<br> Alas = '.$alas;
+        echo '<br> Sisi Miring = '.$sisi;
+
+        echo '<br> Maka Kelilingnya adalah ' .$keliling;
     }
     ?>
 </body>
